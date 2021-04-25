2e57aed2bc79c877abe3c88252e4fc066908f3de
juzhiyuan <juzhiyuan@apache.org>
2021-04-25 09:26:12 +0800

    style: update UI

--- views/Home/components/HomeCTA/index.tsx
+++ views/Home/components/HomeCTA/index.tsx
@@ -12,75 +12,53 @@ type Props = {
 
 const App: NextPage<Props, any> = ({ t }) => {
   return (
-    <Box as="section">
-      <Box
-        maxW="3xl"
+    <Box as="section" py="12" bg={useColorModeValue('gray.50', 'gray.800')} borderBottom="1px solid #dee2e4">
+      <Stack
+        spacing="6"
+        direction={{ base: 'column', md: 'row' }}
+        align={{ md: 'center' }}
+        justify="space-between"
+        maxW={{ base: 'xl', md: '7xl' }}
         mx="auto"
-        px={{ base: '6', lg: '8' }}
-        py={{ base: '16', sm: '20' }}
-        textAlign="center"
+        px={{ base: '6', md: '8' }}
       >
-        <Text fontWeight="semibold" color={useColorModeValue('blue.600', 'blue.200')}>
-          {t("home-text25")}
-        </Text>
-        <Heading
-          my="4"
-          as="h2"
-          fontSize={{ base: '4xl', md: '6xl' }}
-          fontWeight="extrabold"
-          letterSpacing="tight"
-          lineHeight="1.2"
-        >
-          {t("home-text26")}
-          <Box
-            as="mark"
-            bg="unset"
-            color={useColorModeValue('blue.600', 'blue.200')}
-            whiteSpace="nowrap"
-          >
-            {t("home-text27")}
-          </Box>
-          {t("home-text28")}
-        </Heading>
-        <Text fontSize="lg" maxW="xl" mx="auto">
-          {t("home-text31")}
-        </Text>
-        <Stack
-          direction={{ base: 'column', sm: 'row' }}
-          mt="10"
-          justify="center"
-          spacing={{ base: '3', md: '5' }}
-          maxW="md"
-          mx="auto"
-        >
+        <Box>
+          <Text mb="2" fontSize="lg" fontWeight="semibold" color={useColorModeValue('gray.600', 'gray.400')}>
+            {t("home-text25")}
+          </Text>
+          <Heading size="xl" fontWeight="extrabold" maxW="20ch">
+            {t("home-text26")}
+            <Box
+              as="mark"
+              bg="unset"
+              color={useColorModeValue('blue.600', 'blue.200')}
+              whiteSpace="nowrap"
+            >
+              {t("home-text27")}
+            </Box>
+            {t("home-text28")}
+          </Heading>
+        </Box>
+        <Stack direction={{ base: 'column', sm: 'row' }} spacing={{ base: '2', sm: '4' }}>
           <Button
+            size="lg"
+            colorScheme="blue"
             as="a"
             href="/form-api7-trial"
-            size="lg"
-            h="16"
-            px="10"
-            colorScheme="blue"
-            fontWeight="bold"
-            flex={{ md: '1' }}
             _hover={{ color: "var(--chakra-colors-white)", background: "var(--chakra-colors-blue-600)", textDecoration: "none" }}
           >
             {t("home-text29")}
           </Button>
           <Button
-            as="a"
-            flex={{ md: '1' }}
-            variant="outline"
-            href="mailto:wenming@api7.ai"
             size="lg"
-            h="16"
-            px="10"
-            fontWeight="bold"
-            _hover={{ color: "#1A202C", background: "var(--chakra-colors-gray-100)", textDecoration: "none" }}
+            as="a"
+            href="mailto:wenming@api7.ai"
+            _hover={{ color: "#1A202C", background: "var(--chakra-colors-gray-200)", textDecoration: "none" }}
           >
             {t("home-text30")}
           </Button>
         </Stack>
-      </Box>
+      </Stack>
     </Box>
   )
 }
