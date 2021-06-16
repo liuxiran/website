9ca49419caf91d0c45d069e2d0dbf51ba291a58f
liuxiran <belovedxixi@126.com>
2021-06-16 17:23:51 +0800

    fix: auto play

--- views/Home/components/HomeTestimonials/index.tsx
+++ views/Home/components/HomeTestimonials/index.tsx
@@ -51,6 +51,10 @@ const HomeTestimonials: NextPage<Props, any> = ({ t }) => {
 		slidesPerView: 1,
 		spaceBetween: 30,
 		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false
+		},
 		pagination: {
 			el: '.swiper-pagination',
 			clickable: true,
